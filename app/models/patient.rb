c10161700d1db31147cda5fbcf942aab6e8c8a53
Kevin McLoughlin <kevinmcloughlin2010@gmail.com>
2015-07-28 16:20:35 -0400

    update Patient model and view with password

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -5,4 +5,10 @@ class Patient < ActiveRecord::Base
 
   authenticates_with_sorcery!
 
+  validates :password, length: { minimum: 7 }
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
+
+  validates :email, uniqueness: true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  resources :patients
+  resources :patients do
+    resources :health_records
+    resources :health_status_updates
+  end
   resources :doctors
 end
