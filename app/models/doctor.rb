108ae2b98c0b9ec9690c8d2e39c15687a47fb708
Kevin McLoughlin <kevinmcloughlin2010@gmail.com>
2015-07-28 14:09:18 -0400

    fix syntax error

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,6 +1,6 @@
 class Doctor < ActiveRecord::Base
-  has_one :health_record through: :patient
-  has_many :health_status_updates through: :patients
+  has_one :health_record, through: :patient
+  has_many :health_status_updates, through: :patients
   has_and_belongs_to_many :patients
 
 end
