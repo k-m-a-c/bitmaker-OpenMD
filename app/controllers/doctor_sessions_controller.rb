de16e569848b3e5ef4d9968642c591c5c163f68c
shumayel.khan@mail.utoronto.ca <shumayel.khan@mail.utoronto.ca>
2015-07-28 17:48:37 -0400

    Added actions to Doctor and Patient Controllers

--- app/controllers/doctor_sessions_controller.rb
+++ app/controllers/doctor_sessions_controller.rb
@@ -1,2 +1,21 @@
 class DoctorSessionsController < ApplicationController
+
+    def new
+        @doctor = Doctor.new
+    end
+
+    def create
+      if @doctor = login(params[:email], params[:password])
+          redirect_back_or_to(:doctors, notice: 'Login successful')
+      else
+          flash.now[:alert] = 'Login failed'
+          render action: 'new'
+      end
+    end
+
+    def destroy
+      logout
+      redirect_to(:doctors, notice: 'Logged out!')
+    end
+
 end
--- app/controllers/patient_sessions_controller.rb
+++ app/controllers/patient_sessions_controller.rb
@@ -1,2 +1,20 @@
 class PatientSessionsController < ApplicationController
+
+    def new
+        @patient = Patient.new
+    end
+
+    def create
+      if @patient = login(params[:email], params[:password])
+          redirect_back_or_to(:patients, notice: 'Login successful')
+      else
+          flash.now[:alert] = 'Login failed'
+          render action: 'new'
+      end
+    end
+
+    def destroy
+      logout
+      redirect_to(:patients, notice: 'Logged out!')
+    end
 end
