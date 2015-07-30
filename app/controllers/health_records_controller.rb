9ec2a57b0b6d944aa1c6279d33337a9d3f45d4e0
shumayel.khan@mail.utoronto.ca <shumayel.khan@mail.utoronto.ca>
2015-07-30 14:16:20 -0400

    Added requisite actions to respective H.R. and H.S.U controllers

--- app/controllers/health_records_controller.rb
+++ app/controllers/health_records_controller.rb
@@ -1,2 +1,41 @@
 class HealthRecordsController < ApplicationController
+
+  def index
+    @healthrecords = HealthRecord.all
+  end
+
+  def show
+    @healthrecord = HealthRecord.find(params[:id])
+  end
+
+  def new
+    @healthrecord = HealthRecord.new
+  end
+
+  def edit
+  end
+
+  def create
+    @healthrecord = HealthRecord.new(healthrecord_params)
+
+    if @healthrecord.save
+      redirect_to healthrecords_url
+    else
+      render :new
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+      @healthrecord = HealthRecord.find(params[:id])
+      @healthrecord.destroy
+      redirect_to healthrecords_url
+  end
+
+  private
+  def healthrecords_params
+    params.require(:healthrecords).permit()
+  end
 end
--- app/controllers/health_status_updates_controller.rb
+++ app/controllers/health_status_updates_controller.rb
@@ -1,2 +1,36 @@
 class HealthStatusUpdatesController < ApplicationController
+
+  def index
+    @healthstatusupdates = HealthStatusUpdates.all
+  end
+
+  def show
+    @healthstatusupdate = HealthStatusUpdate.find(params[:id])
+  end
+
+  def new
+    @healthstatusupdate = HealthStatusUpdate.new
+  end
+
+  def edit
+  end
+
+  def create
+    @healthstatusupdate = HealthStatusUpdate.new(healthrecord_params)
+
+    if @healthstatusupdate.save
+      redirect_to healthstatusupdates_url
+    else
+      render :new
+    end
+  end
+
+  def update
+  end
+
+  private
+  def healthstatusupdates_params
+    params.require(:healthstatusupdates).permit()
+  end
+
 end
