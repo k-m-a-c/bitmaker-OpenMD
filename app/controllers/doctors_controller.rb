e5ab90c3920f4b0a1fc1b9fdf4d6c56165e8dce9
Kevin McLoughlin <kevinmcloughlin2010@gmail.com>
2015-08-02 21:36:34 -0400

    add validations to Patient model

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,4 +1,6 @@
 class DoctorsController < ApplicationController
+  before_filter :authenticate_doctor!
+
   def index
     @doctors = Doctor.all
   end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -9,4 +9,22 @@ class Patient < ActiveRecord::Base
 
   accepts_nested_attributes_for :health_record
   accepts_nested_attributes_for :health_status_updates
+
+  validates :first_name, presence: true
+  validates :first_name, presence: true
+  validates :phone_number, presence: true, numericality: true
+  validates :healthcard_number, presence: true
+  validates :gender, presence: true
+  validates :city, presence: true
+  validates :country, presence: true
+
+  validate :date_of_birth_is_in_the_past?
+
+
+  def date_of_birth_is_in_the_past?
+    if date_of_birth.present? && date_of_birth > Date.tomorrow
+      errors.add(:date_of_birth, "your date of birth must be in the past")
+    end
+  end
+
 end
